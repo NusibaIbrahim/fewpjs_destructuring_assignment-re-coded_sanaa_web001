0f44eb3a7a2564851b092288be69adea389a4b6a
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-14 12:05:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ console.log(moo, neigh, baa, oink, cluck);
 const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(" ");
 console.log(Bessie, Dolly, Babe, Little);
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-
+const [black_and_white, , black, pink] = farmAnimals.split(" ");
+console.log(black_and_white, black, pink);
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
